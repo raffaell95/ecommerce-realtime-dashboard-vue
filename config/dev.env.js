77ad3040c412fa162d9ef3432df31eb56b56af54
raffaell95 <raffaell95@live.com>
2020-05-26 21:44:22 -0300

    first commit

---
+++ config/dev.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: '"development"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"https://ecommerce-api-adonis.herokuapp.com/v1"',
+  WS_ENDPOINT: '"ws://ecommerce-api-adonis.herokuapp.com/"'
+}
---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"production"',
+  BASE_API: '"https://ecommerce-api-adonis.herokuapp.com/v1"',
+  WS_ENDPOINT: '"wss://ecommerce-api-adonis.herokuapp.com"'
+}
